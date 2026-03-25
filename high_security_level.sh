c6b114e7e8a4d55cb3958d4cd91579627b9f79d4
Wiktoria Blomgren Strandberg <102546496+wikblo-0@users.noreply.github.com>
2026-03-25 18:14:12 +0100

    Create high_security_level.sh

---
+++ high_security_level.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+SECURITY="high" #security level in DVWA
+USER="admin" #username to log in with
+PASS="password" #password for selected user
+
+rm cookies.txt #removes old file with cookies
+
+#saves login cookies and login page html to local files
+curl -s -c cookies.txt \
+-b "security=$SECURITY" \
+http://192.168.56.105/DVWA/login.php \
+>login.html
+
+TOKEN=$(grep -oP "name='user_token' value='\K[^']+" login.html) #saves user token variable found in login html
+PHPSESSID=$(awk '$6=="PHPSESSID" {print $7}' cookies.txt) #saves PHP session ID found in cookies
+
+#logs in using cookies and user token
+curl -s -b cookies.txt \
+-b "security=$SECURITY" \
+-d "username=$USER&password=$PASS&user_token=$TOKEN&Login=Login" \
+http://192.168.56.105/DVWA/login.php
+
+
+PHRASE="success" #phrase to submit
+
+REV=$(echo "$PHRASE" | rev) #step 1
+
+STEP2=$(echo -n "XX$REV" | sha256sum | awk '{print $1}') #step 2
+
+TOKEN=$(echo -n "$STEP2""ZZ" | sha256sum | awk '{print $1}') #step 3
+
+#sends request and saves result in local file
+{
+echo "Current security level: $SECURITY"
+echo "Token value: $TOKEN"
+echo "Attempting to submit the word $PHRASE..."
+
+RESPONSE=$(curl -s -X POST \
+ -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
+ -d "phrase=$PHRASE&token=$TOKEN&send=Submit" \
+http://192.168.56.105/DVWA/vulnerabilities/javascript/)
+
+if echo "$RESPONSE" | grep -q "Well done!"; then
+    echo "Attempt result -> SUCCESS!"
+else
+    echo "Attempt result -> FAILURE"
+fi
+}> /home/kali/javascript_high.log

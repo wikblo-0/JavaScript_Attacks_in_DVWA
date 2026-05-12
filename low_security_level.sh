29953393ef57615340055b3b695b7936c6879604
Wiktoria Blomgren Strandberg <102546496+wikblo-0@users.noreply.github.com>
2026-05-12 15:59:33 +0200

    Refactor curl commands for improved readability

--- low_security_level.sh
+++ low_security_level.sh
@@ -8,18 +8,18 @@ rm cookies.txt #removes old file with cookies
 
 #saves login cookies and login page html to local files
 curl -s -c cookies.txt \
--b "security=$SECURITY" \
-http://192.168.56.105/DVWA/login.php \
->login.html
+  -b "security=$SECURITY" \
+  http://192.168.56.105/DVWA/login.php \
+  >login.html
 
 TOKEN=$(grep -oP "name='user_token' value='\K[^']+" login.html) #saves user token variable found in login html
 PHPSESSID=$(awk '$6=="PHPSESSID" {print $7}' cookies.txt) #saves PHP session ID found in cookies
 
 #logs in using cookies and user token
 curl -s -b cookies.txt \
--b "security=$SECURITY" \
--d "username=$USER&password=$PASS&user_token=$TOKEN&Login=Login" \
-http://192.168.56.105/DVWA/login.php
+  -b "security=$SECURITY" \
+  -d "username=$USER&password=$PASS&user_token=$TOKEN&Login=Login" \
+  http://192.168.56.105/DVWA/login.php
 
 
 PHRASE="success" #phrase to submit
@@ -37,13 +37,13 @@ echo "Token value: $TOKEN"
 echo "Attempting to submit the word $PHRASE..."
 
 RESPONSE=$(curl -s -X POST \
- -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
- -d "phrase=$PHRASE&token=$TOKEN&send=Submit" \
-http://192.168.56.105/DVWA/vulnerabilities/javascript/)
+  -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
+  -d "phrase=$PHRASE&token=$TOKEN&send=Submit" \
+  http://192.168.56.105/DVWA/vulnerabilities/javascript/)
 
 if echo "$RESPONSE" | grep -q "Well done!"; then
-    echo "Attempt result -> SUCCESS!"
+  echo "Attempt result -> SUCCESS!"
 else
-    echo "Attempt result -> FAILURE"
+  echo "Attempt result -> FAILURE"
 fi
 }> /home/kali/javascript_low.log
